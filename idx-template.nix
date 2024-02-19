37b905e103cf3a3cf6d5b6bf65a5e8de0c7476e4
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2024-02-19 13:59:33 +0100

    fix: adjustments

--- idx-template.nix
+++ idx-template.nix
@@ -8,10 +8,9 @@
     npm init -y
     npm install @11ty/eleventy --save-dev
     echo '# Hello World!' > index.md
-    cd ..
-    mkdir -p "$WS_NAME/.idx/"
-    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
-    chmod -R +w "$WS_NAME"
-    mv "$WS_NAME" "$out"
+    mkdir -p ".idx/"
+    cp ${./../dev.nix} ".idx/dev.nix"
+    chmod -R +w "../$WS_NAME"
+    mv "../$WS_NAME" "$out"
   '';
 }
